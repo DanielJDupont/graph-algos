59318ec8d468864a21dd87498a592ddda4c7e5d8
DanielJDupont <ddupont2@uwo.ca>
2021-02-12 18:41:37 -0500

    added reset button

---
+++ components/matrix/index.dataTypes.ts
@@ -0,0 +1,9 @@
+/*
+ A square is one of the matrix elements.
+ isProcessed false means that the square has not been visited by the algorithm yet.
+ isProcessed true means that the square has been processed by the algorithm.
+*/
+export interface Square {
+  id: string;
+  isProcessed: boolean;
+}
--- components/matrix/index.tsx
+++ components/matrix/index.tsx
@@ -1,26 +1,87 @@
-import { useEffect } from 'react';
-import { useMediaQuery } from './index.hooks';
+import { useState } from 'react';
+import clsx from 'clsx';
 
+import { Square } from './index.dataTypes';
+import { useMediaQuery } from './index.hooks';
 import styles from './index.module.scss';
 
 export const Matrix = () => {
-  // Use a grid that fits on most phones if the width of the screen is small, otherwise use a larger matrix.
-  const isScreenSmall = useMediaQuery(900);
-  const matrix = isScreenSmall
-    ? [...Array(10).map(() => [...Array(6)])]
-    : [...Array(20).map(() => [...Array(20)])];
+  const [matrix, setMatrix] = useState(
+    [...Array(30)].map((_, i) =>
+      [...Array(20)].map((_, j) => ({
+        id: i + ' ' + j,
+        isProcessed: false,
+      }))
+    )
+  );
 
-  console.log(matrix);
+  const resetMatrix = () => {
+    setMatrix(
+      [...Array(30)].map((_, i) =>
+        [...Array(20)].map((_, j) => ({
+          id: i + ' ' + j,
+          isProcessed: false,
+        }))
+      )
+    );
+  };
+
+  const depthFirstSearch = async (i: number, j: number) => {
+    console.log('running');
+    // Delay so it appears to animate for the user.
+    await setTimeout(() => {}, 100);
+
+    // Ensure that the current grid square we are on is defined at i and j.
+    if (i >= 0 && i < matrix.length) {
+      if (j >= 0 && j < matrix[i].length) {
+        if (matrix[i][j].isProcessed) return;
+        matrix[i][j].isProcessed = true;
+
+        setMatrix(
+          matrix.map((row, _i) =>
+            row.map((square, _j) => {
+              if (i === _i && j === _j) square.isProcessed = true;
+              return square;
+            })
+          )
+        );
+
+        await setTimeout(() => {
+          depthFirstSearch(i, j + 1);
+          depthFirstSearch(i + 1, j);
+          depthFirstSearch(i, j - 1);
+          depthFirstSearch(i - 1, j);
+        }, 100);
+        // Travel top, right, bottom, left
+      }
+    }
+
+    return;
+  };
 
   return (
     <div className={styles.matrixContainer}>
       <div className={styles.matrixTitle}>Square Grid</div>
+
+      <div className={styles.buttonContainer}>
+        <button onClick={() => depthFirstSearch(0, 0)}>
+          Run Depth First Search
+        </button>
+        <button onClick={() => resetMatrix()}>Reset Matrix</button>
+      </div>
+
       {/* A grid that holds all of the squares. */}
       <div className={styles.squareContainer}>
         {matrix.map((row) => (
           <div className={styles.row}>
-            {matrix.map((column) => (
-              <div className={styles.square} />
+            {row.map((square) => (
+              <div
+                key={square.id}
+                className={clsx(
+                  styles.unprocessedSquare,
+                  square.isProcessed && styles.processedSquare
+                )}
+              />
             ))}
           </div>
         ))}
