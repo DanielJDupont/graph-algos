8afde6e2859600042255692a58d8184be128d909
DanielJDupont <ddupont2@uwo.ca>
2021-02-13 21:10:38 -0500

    added navbar with menu button

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,8 +1,19 @@
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
 import styles from './index.module.scss';
 
 export const Navbar = () => {
   return (
     <div className={styles.navbarContainer}>
+      <IconButton
+        className={styles.menuButton}
+        edge="start"
+        color="inherit"
+        aria-label="menu"
+      >
+        <MenuIcon />
+      </IconButton>
       <div className={styles.navbarTitle}>
         Interactive Graph Algorithm Visualizer
       </div>
