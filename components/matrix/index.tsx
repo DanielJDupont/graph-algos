a488f666c8b51d984aeb3ecc2518c19ceeb16803
DanielJDupont <ddupont2@uwo.ca>
2021-03-06 21:18:41 -0500

    added plan on how to approach this problem.

--- components/matrix/index.tsx
+++ components/matrix/index.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from 'react';
 import clsx from 'clsx';
-import Anime from 'rollup-practice';
+import Anime from 'react-animejs-wrapper';
 
 import { Button } from '@material-ui/core';
 
@@ -19,28 +19,6 @@ export const Matrix = () => {
   );
 
   useEffect(() => {}, []);
-
-  const resetMatrix = () => {
-    setProcessList([]);
-    setMatrix(
-      [...Array(20)].map((_, i) =>
-        [...Array(30)].map((_, j) => ({
-          id: i + ' ' + j,
-          isProcessed: false,
-          isDisplayed: false,
-        }))
-      )
-    );
-  };
-
-  const displayResults = () => {
-    /*
-    The plan here is for our depth first search to have added some number like 100ms, 200ms, 300ms...
-    As animation play delays on a library like framer motion, or a more modern one.
-
-    So the animation library will just display the entire grid all at once and it will automatically animate everything.
-    */
-  };
 
   const depthFirstSearch = async (i: number, j: number) => {
     // Ensure that the current grid square we are on is defined at i and j.
@@ -61,62 +39,24 @@ export const Matrix = () => {
     return;
   };
 
-  const animatedSquaresRef = useRef(null);
-
-  const restart = () => {
-    if (animatedSquaresRef) {
-      animatedSquaresRef.current.restart();
-    }
-  };
-
   return (
-    <div className={styles.matrixContainer}>
-      <button onClick={restart}>Reset Animation</button>
-      <div className={styles.matrixTitle}>Square Grid</div>
-      <Anime
-        ref={animatedSquaresRef}
-        animeConfig={{
-          translateX: 250,
-          delay: function (_, i) {
-            return i * 100;
-          },
-          loop: true,
-          direction: 'alternate',
-          easing: 'easeInOutSine',
-          autoplay: true,
-        }}
-      >
-        <div className={styles.element}>1</div>
-        <div className={styles.element}>2</div>
-        <div className={styles.element}>3</div>
-      </Anime>
-
-      <div className={styles.buttonContainer}>
-        <Button color="primary" onClick={() => depthFirstSearch(0, 0)}>
-          Run Depth First Search
-        </Button>
-        <Button color="primary" onClick={() => resetMatrix()}>
-          Reset Matrix
-        </Button>
-      </div>
-      {processList.length}
-      <div className={styles.squareContainer}>
-        {matrix.map((row) => (
-          <div className={styles.row}>
-            {row.map((square) => (
-              <div
-                key={square.id}
-                className={clsx(
-                  styles.unprocessedSquare,
-                  square.isProcessed && styles.processedSquare
-                )}
-              >
-                {square.id}
-              </div>
-            ))}
-          </div>
-        ))}
-      </div>
+    <div className={styles.container}>
+      <div className={styles.matrixContainer}>Square Grid</div>
     </div>
   );
 };
+
+/*
+Phase 1: User Input, the user may select the start and the user may select the end.
+The data structure for the matrix must be decided upon.
+The last square to be selected must be selectable.
+The finishing square must be selected as well.
+There must be buttons to start the traversal as well.
+
+Phase 2: Calculate the order in which all squares are visited, giving them numbers from 1 to n.
+We then must have an idea of what the data structure will be, probably the same as above but with a field for tracking visited.
+
+Phase 3: Show the traversal. 
+To do this make a new matrix but with <Anime></Anime> tags for each element, with delays set to 200ms * visited.
+You can simply traverse everything 1 time in O(n) time to construct the new matrix.
+*/
