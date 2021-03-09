6ad16adaadc2b331ff3123e7dbacaf8b44b197dc
DanielJDupont <ddupont2@uwo.ca>
2021-03-08 19:03:53 -0500

    removed unused  elements from navbar

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -6,19 +6,9 @@ import styles from './index.module.scss';
 export const Navbar = () => {
   return (
     <div className={styles.navbarContainer}>
-      <IconButton
-        className={styles.menuButton}
-        edge="start"
-        color="inherit"
-        aria-label="menu"
-      >
-        <MenuIcon />
-      </IconButton>
       <div className={styles.navbarTitle}>
         Interactive Graph Algorithm Visualizer
       </div>
-      <div className={styles.navbarItem}>Matrix Layout</div>
-      <div className={styles.navbarItem}>Vertexes and Edges Layout</div>
     </div>
   );
 };
