6a9dfb0ce977584de45cabc7fd238afa1564433c
DanielJDupont <ddupont2@uwo.ca>
2021-03-10 16:41:53 -0500

    centered controls nicely

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -35,112 +35,114 @@ export const Navbar = () => {
         Interactive Graph Algorithm Visualizer
       </div>
 
-      <Button
-        className={clsx(
-          styles.button,
-          mouseMode === MouseMode.StartingPoint && styles.buttonActive
-        )}
-        disabled={isDisplayingAlgorithm}
-        variant="contained"
-        onClick={() => {
-          if (mouseMode !== MouseMode.StartingPoint) {
-            setMouseMode(MouseMode.StartingPoint);
-          } else if (mouseMode === MouseMode.StartingPoint) {
-            setMouseMode(MouseMode.NormalPoint);
+      <div className={styles.controlsContainer}>
+        <Button
+          className={clsx(
+            styles.button,
+            mouseMode === MouseMode.StartingPoint && styles.buttonActive
+          )}
+          disabled={isDisplayingAlgorithm}
+          variant="contained"
+          onClick={() => {
+            if (mouseMode !== MouseMode.StartingPoint) {
+              setMouseMode(MouseMode.StartingPoint);
+            } else if (mouseMode === MouseMode.StartingPoint) {
+              setMouseMode(MouseMode.NormalPoint);
+            }
+          }}
+        >
+          <DirectionsRunOutlined className={styles.icon} />
+          Starting Point
+        </Button>
+
+        <Button
+          className={clsx(
+            styles.button,
+            mouseMode === MouseMode.EndingPoint && styles.buttonActive
+          )}
+          disabled={isDisplayingAlgorithm}
+          variant="contained"
+          onClick={() => {
+            if (mouseMode !== MouseMode.EndingPoint) {
+              setMouseMode(MouseMode.EndingPoint);
+            } else if (mouseMode === MouseMode.EndingPoint) {
+              setMouseMode(MouseMode.NormalPoint);
+            }
+          }}
+        >
+          <FlagOutlined className={styles.icon} />
+          Ending Point
+        </Button>
+
+        <Select
+          className={styles.selector}
+          value={algorithmChoice}
+          disabled={isDisplayingAlgorithm}
+          onChange={(event: any) => {
+            setAlgorithmChoice(event.target.value);
+            console.log(event.target.value);
+          }}
+        >
+          <MenuItem value={AlgorithmChoice.ChooseYourAlgorithm} disabled>
+            Choose Your Algorithm
+          </MenuItem>
+
+          <MenuItem value={AlgorithmChoice.DepthFirstSearch}>
+            Depth First Search
+          </MenuItem>
+
+          <MenuItem value={AlgorithmChoice.BreadthFirstSearch}>
+            Breadth First Search
+          </MenuItem>
+        </Select>
+
+        <Button
+          className={styles.button}
+          variant="contained"
+          disabled={
+            algorithmChoice === AlgorithmChoice.ChooseYourAlgorithm ||
+            isDisplayingAlgorithm
           }
-        }}
-      >
-        <DirectionsRunOutlined className={styles.icon} />
-        Starting Point
-      </Button>
+          onClick={() => {
+            if (algorithmChoice === AlgorithmChoice.BreadthFirstSearch) {
+              breadthFirstSearch(startSquareID, matrix, setProcessList);
+            } else if (algorithmChoice === AlgorithmChoice.DepthFirstSearch) {
+              depthFirstSearch(startSquareID, matrix, setProcessList);
+            }
 
-      <Button
-        className={clsx(
-          styles.button,
-          mouseMode === MouseMode.EndingPoint && styles.buttonActive
-        )}
-        disabled={isDisplayingAlgorithm}
-        variant="contained"
-        onClick={() => {
-          if (mouseMode !== MouseMode.EndingPoint) {
-            setMouseMode(MouseMode.EndingPoint);
-          } else if (mouseMode === MouseMode.EndingPoint) {
-            setMouseMode(MouseMode.NormalPoint);
-          }
-        }}
-      >
-        <FlagOutlined className={styles.icon} />
-        Ending Point
-      </Button>
+            setIsDisplayingAlgorithm(true);
+          }}
+        >
+          <PlayCircleOutline className={styles.icon} />
+          Start Animation
+        </Button>
 
-      <Select
-        className={styles.selector}
-        value={algorithmChoice}
-        disabled={isDisplayingAlgorithm}
-        onChange={(event: any) => {
-          setAlgorithmChoice(event.target.value);
-          console.log(event.target.value);
-        }}
-      >
-        <MenuItem value={AlgorithmChoice.ChooseYourAlgorithm} disabled>
-          Choose Your Algorithm
-        </MenuItem>
-
-        <MenuItem value={AlgorithmChoice.DepthFirstSearch}>
-          Depth First Search
-        </MenuItem>
-
-        <MenuItem value={AlgorithmChoice.BreadthFirstSearch}>
-          Breadth First Search
-        </MenuItem>
-      </Select>
-
-      <Button
-        className={styles.button}
-        variant="contained"
-        disabled={
-          algorithmChoice === AlgorithmChoice.ChooseYourAlgorithm ||
-          isDisplayingAlgorithm
-        }
-        onClick={() => {
-          if (algorithmChoice === AlgorithmChoice.BreadthFirstSearch) {
-            breadthFirstSearch(startSquareID, matrix, setProcessList);
-          } else if (algorithmChoice === AlgorithmChoice.DepthFirstSearch) {
-            depthFirstSearch(startSquareID, matrix, setProcessList);
-          }
-
-          setIsDisplayingAlgorithm(true);
-        }}
-      >
-        <PlayCircleOutline className={styles.icon} />
-        Start Animation
-      </Button>
-
-      <Button
-        className={clsx(styles.button)}
-        disabled={!isDisplayingAlgorithm}
-        variant="contained"
-        onClick={() => {
-          // Wipe out the matrix so nothing is processed anymore.
-          setMatrix(
-            matrix.map((row) =>
-              row.map((square) => {
-                if (!square.isBlocked) {
-                  return { ...square, isProcessed: false };
-                } else {
-                  // Squares that are blocked must also be marked as having been processed.
-                  return { ...square, isProcessed: true, isBlocked: true };
-                }
-              })
-            )
-          );
-          setIsDisplayingAlgorithm(false);
-          setProcessList([]);
-        }}
-      >
-        <ReplayOutlined className={styles.icon} />
-        Reset
-      </Button>
+        <Button
+          className={clsx(styles.button)}
+          disabled={!isDisplayingAlgorithm}
+          variant="contained"
+          onClick={() => {
+            // Wipe out the matrix so nothing is processed anymore.
+            setMatrix(
+              matrix.map((row) =>
+                row.map((square) => {
+                  if (!square.isBlocked) {
+                    return { ...square, isProcessed: false };
+                  } else {
+                    // Squares that are blocked must also be marked as having been processed.
+                    return { ...square, isProcessed: true, isBlocked: true };
+                  }
+                })
+              )
+            );
+            setIsDisplayingAlgorithm(false);
+            setProcessList([]);
+          }}
+        >
+          <ReplayOutlined className={styles.icon} />
+          Reset
+        </Button>
+      </div>
     </div>
   );
 };
