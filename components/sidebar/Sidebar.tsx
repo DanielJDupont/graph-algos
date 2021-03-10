02d45f7f9fe824173fa8a4f0ea0b7d931c0eacfa
DanielJDupont <ddupont2@uwo.ca>
2021-03-10 18:13:53 -0500

    isOpen stuff

--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -1,11 +1,25 @@
-import { KeyboardArrowRight } from '@material-ui/icons';
+import { useState } from 'react';
+import clsx from 'clsx';
+
+import { KeyboardArrowRight, KeyboardArrowLeft } from '@material-ui/icons';
 
 import styles from './Sidebar.module.scss';
 
 export const Sidebar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className={styles.container}>
-      <KeyboardArrowRight className={styles.icon} />
+    <div
+      className={clsx(
+        !isOpen && styles.container,
+        isOpen && styles.openContainer
+      )}
+      onClick={() => setIsOpen(!isOpen)}
+    >
+      <div className={styles.sidebarContents}>
+        {!isOpen && <KeyboardArrowRight className={styles.icon} />}
+        {isOpen && <KeyboardArrowLeft className={styles.icon} />}
+      </div>
     </div>
   );
 };
