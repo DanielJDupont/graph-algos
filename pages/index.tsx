4df565b8f04018fc06f3d929959bfc217f0e4a8a
DanielJDupont <ddupont2@uwo.ca>
2021-03-10 16:47:40 -0500

    removed margin on navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,21 @@
 import Head from 'next/head';
 import { Matrix } from '../components/matrix';
 import { Navbar } from '../components/navbar';
+import { Sidebar } from '../components/sidebar/Sidebar';
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>Graph Algos</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <Navbar />
-      <Matrix />
+      <div style={{ position: 'relative' }}>
+        <Sidebar />
+        <Matrix />
+      </div>
     </>
   );
 }
