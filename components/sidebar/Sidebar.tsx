5fffadce8ddfcccb280b08a78e74278721af4d73
DanielJDupont <ddupont2@uwo.ca>
2021-03-13 21:51:20 -0500

    implemented custom sidebar

--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -6,7 +6,7 @@ import { KeyboardArrowRight, KeyboardArrowLeft } from '@material-ui/icons';
 import styles from './Sidebar.module.scss';
 
 export const Sidebar = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(true);
 
   return (
     <div
@@ -16,9 +16,25 @@ export const Sidebar = () => {
       )}
       onClick={() => setIsOpen(!isOpen)}
     >
-      <div className={styles.sidebarContents}>
+      <div className={styles.sidebar}>
         {!isOpen && <KeyboardArrowRight className={styles.icon} />}
         {isOpen && <KeyboardArrowLeft className={styles.icon} />}
+
+        <div
+          className={clsx(
+            isOpen ? styles.sidebarContents : styles.sidebarFaded
+          )}
+        >
+          <div>Set the starting point.</div>
+          <div>Set the ending point.</div>
+          <div>
+            Create walls to alter the path of your algorithm by clicking and
+            dragging.
+          </div>
+          <div>Automatically generate mazes or clear all walls.</div>
+          <div>Select an algorithm and run your algorithm.</div>
+          <div>Reset and change what you like.</div>
+        </div>
       </div>
     </div>
   );
