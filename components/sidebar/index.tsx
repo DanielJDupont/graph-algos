98f76ce3c1e1a8372aa04721ddf1469dc6e15e27
DanielJDupont <ddupont2@uwo.ca>
2021-03-14 19:21:11 -0400

    adjusted tutorial and added some images showing how it works

--- components/sidebar/index.tsx
+++ components/sidebar/index.tsx
@@ -17,22 +17,47 @@ export const Sidebar = () => {
       onClick={() => setIsOpen(!isOpen)}
     >
       <div className={styles.sidebar}>
-        {!isOpen && <KeyboardArrowRight className={styles.icon} />}
-        {isOpen && <KeyboardArrowLeft className={styles.icon} />}
+        <div className={styles.topContainer}>
+          <div className={styles.title}>Tutorial</div>
+          {!isOpen && <KeyboardArrowRight className={styles.icon} />}
+          {isOpen && <KeyboardArrowLeft className={styles.icon} />}
+        </div>
 
         <div
           className={clsx(
             isOpen ? styles.sidebarContents : styles.sidebarFaded
           )}
         >
-          <div>Set the starting point.</div>
-          <div>Set the ending point.</div>
           <div>
-            Create walls to alter the path of your algorithm by clicking and
-            dragging.
+            <div className={styles.tutorialText}>Set the starting point.</div>
+            <img src="startPoint.gif" />
           </div>
+
+          <div>
+            <div className={styles.tutorialText}>Set the ending point.</div>
+            <img src="endPoint.gif" />
+          </div>
+
+          <div>
+            <div className={styles.tutorialText}>
+              Create walls to alter the path of your algorithm by clicking and
+              dragging.
+            </div>
+            <img src="walls.gif" />
+          </div>
+
           <div>Automatically generate mazes or clear all walls.</div>
-          <div>Select an algorithm and run your algorithm.</div>
+
+          <div>
+            <div className={styles.tutorialText}>
+              Select an algorithm and run.
+            </div>
+            <img
+              src="chooseyouralgo.gif"
+              style={{ width: '226px', height: '141px' }}
+            />
+          </div>
+
           <div>Reset and change what you like.</div>
         </div>
       </div>
