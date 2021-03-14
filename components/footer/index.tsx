6a0108d68a9e23c8cfd205abc467080bdc2e78fb
DanielJDupont <ddupont2@uwo.ca>
2021-03-14 00:27:48 -0500

    quality

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -6,7 +6,28 @@ export const Footer = () => {
       <div className={styles.createdBy}>
         Created by Daniel Dupont | Graph Algos © 2020
       </div>
-      <div></div>
+      <div>
+        <img
+          alt="GitHub"
+          src="https://img.shields.io/github/license/danieljdupont/graph-algos?logo=MIT&color=9cf"
+        />
+        <img
+          alt="GitHub code size in bytes"
+          src="https://img.shields.io/github/languages/code-size/danieljdupont/graph-algos?color=9cf"
+        />
+        <img
+          alt="GitHub language count"
+          src="https://img.shields.io/github/languages/count/danieljdupont/graph-algos?color=9cf"
+        />
+        <img
+          alt="GitHub top language"
+          src="https://img.shields.io/github/languages/top/danieljdupont/graph-algos?color=9cf"
+        />
+        <img
+          alt="Scrutinizer code quality (GitHub/Bitbucket)"
+          src="https://img.shields.io/scrutinizer/quality/g/danieljdupont/graph-algos/main?color=9cf"
+        />
+      </div>
     </div>
   );
 };
